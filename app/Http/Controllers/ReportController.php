b8e0ce47d6525601d505948d92afdf17b92c5091
mjdurandar <mjdurandarjr@gmail.com>
2025-03-10 12:08:39 +0800

    fix csv file

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ReportController extends Controller
 {
@@ -34,49 +35,41 @@ class ReportController extends Controller
     {
         $apiKey = env('MAILCHIMP_API_KEY');
         $server = env('MAILCHIMP_SERVER_PREFIX');
-        $allSubscribers = collect();
-        $offset = 0;
-        $count = 1000; // Number of records per request
-
-        do {
-            $url = "https://{$server}.api.mailchimp.com/3.0/lists/{$audienceId}/members?count={$count}&offset={$offset}";
-            $response = Http::withBasicAuth('anystring', $apiKey)->get($url);
-
-            if ($response->failed()) {
-                return response()->json([]);
-            }
-
-            $data = $response->json();
-            $members = collect($data['members'] ?? []);
-            
-            if ($members->isEmpty()) {
-                break;
-            }
-
-            $subscribers = $members->map(function ($subscriber) {
-                $address = $subscriber['merge_fields']['ADDRESS'] ?? [];
-                
-                return [
-                    'location' => $subscriber['merge_fields']['EVENTSLOCA'] ?? 'N/A',
-                    'email' => $subscriber['email_address'],
-                    'fname' => $subscriber['merge_fields']['FNAME'] ?? 'N/A',
-                    'lname' => $subscriber['merge_fields']['LNAME'] ?? 'N/A',
-                    'phone' => $subscriber['merge_fields']['PHONE'] ?? 'N/A',
-                    'age' => $subscriber['merge_fields']['AGE'] ?? 'N/A',
-                    'gender' => $subscriber['merge_fields']['GENDER'] ?? 'N/A',
-                    'address' => isset($address['addr1']) ? 
-                        trim("{$address['addr1']} {$address['addr2']}, {$address['city']}, {$address['state']} {$address['zip']}, {$address['country']}") 
-                        : '',
-                ];
-            });
-
-            $allSubscribers = $allSubscribers->concat($subscribers);
-            $offset += $count;
-
-        } while ($members->count() === $count);
-
-        return response()->json($allSubscribers);
+    
+        $url = "https://{$server}.api.mailchimp.com/3.0/lists/{$audienceId}/members?count=5000&status=subscribed";
+        
+        $response = Http::withBasicAuth('anystring', $apiKey)->get($url);
+    
+        if ($response->failed()) {
+            Log::error("Mailchimp API request failed: " . $response->body());
+            return response()->json([]);
+        }
+    
+        $data = $response->json();
+        $members = collect($data['members'] ?? []);
+    
+        $subscribers = $members->map(function ($subscriber) {
+            $address = $subscriber['merge_fields']['ADDRESS'] ?? [];
+    
+            return [
+                'location' => $subscriber['merge_fields']['EVENTSLOCA'] ?? 'N/A',
+                'email' => $subscriber['email_address'],
+                'fname' => $subscriber['merge_fields']['FNAME'] ?? 'N/A',
+                'lname' => $subscriber['merge_fields']['LNAME'] ?? 'N/A',
+                'phone' => $subscriber['merge_fields']['PHONE'] ?? 'N/A',
+                'age' => $subscriber['merge_fields']['AGE'] ?? 'N/A',
+                'gender' => $subscriber['merge_fields']['GENDER'] ?? 'N/A',
+                'address' => isset($address['addr1']) ? 
+                    trim("{$address['addr1']} {$address['addr2']}, {$address['city']}, {$address['state']} {$address['zip']}, {$address['country']}") 
+                    : '',
+            ];
+        });
+    
+        Log::info("Total subscribers fetched: " . $subscribers->count());
+    
+        return response()->json($subscribers);
     }
+    
     
 
 }
